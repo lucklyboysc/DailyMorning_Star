0cb53b746ff3c729ee4c8c918429a8b4541c9e9f
lucklyboysc <112157255+lucklyboysc@users.noreply.github.com>
2022-09-17 14:20:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def get_ciba():
     return note_ch, note_en
 
 
-def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, note_ch, note_en):
+def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, now_temperature, note_ch, note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -223,7 +223,7 @@ if __name__ == "__main__":
     users = config["user"]
     # 传入省份和市获取天气信息
     province, city = config["province"], config["city"]
-    weather, max_temperature, min_temperature = get_weather(province, city)
+    weather, max_temperature, min_temperature, now_temperature, = get_weather(province, city)
     # 获取词霸每日金句
     note_ch, note_en = get_ciba()
     # 公众号推送消息
